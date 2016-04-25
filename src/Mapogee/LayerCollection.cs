2915d22e323a7bf716e654bed502e0ece1dfb87b
pauldendulk <pauldendulk@gmail.com>
2016-04-25 22:49:47 +0200

    Adding LayerRemoved and LayerAdded events

--- src/Mapogee/LayerCollection.cs
+++ src/Mapogee/LayerCollection.cs
@@ -7,18 +7,31 @@ namespace Mapogee
     {
         private readonly List<ILayer> _layers = new List<ILayer>();
 
-        public event EventHandler< LayerRemoved;
-        public event EventHandler<EventArgs> LayerAdded;
-        public event EventHandler<EventArgs> LayerMoved;
-
+        public event EventHandler<EventArgs<ILayer>> LayerRemoved;
+        public event EventHandler<EventArgs<ILayer>> LayerAdded;
+        
         public void Add(ILayer layer)
         {
             _layers.Add(layer);
+            OnLayerAdded(layer);
         }
 
         public void Remove(ILayer layer)
         {
             _layers.Remove(layer);
+            OnLayerRemoved(layer);
+        }
+
+        private void OnLayerRemoved(ILayer layer)
+        {
+            var handler = LayerRemoved;
+            handler?.Invoke(this, new EventArgs<ILayer>(layer));
+        }
+
+        private void OnLayerAdded(ILayer layer)
+        {
+            var handler = LayerAdded;
+            handler?.Invoke(this, new EventArgs<ILayer>(layer));
         }
     }
 }
