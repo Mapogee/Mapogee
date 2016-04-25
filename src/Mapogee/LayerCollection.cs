72e53afe73f34d8d68fb74ecf84a116bb66cbb88
pauldendulk <pauldendulk@gmail.com>
2016-04-25 22:25:46 +0200

    Adding LayerCollection

---
+++ src/Mapogee/LayerCollection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Mapogee
+{
+    public class LayerCollection
+    {
+        readonly List<ILayer> _layers = new List<ILayer>();
+
+        public void Add(ILayer layer)
+        {
+            _layers.Add(layer);
+        }
+
+        public void Remove(ILayer layer)
+        {
+            _layers.Remove(layer);
+        }
+    }
+}
