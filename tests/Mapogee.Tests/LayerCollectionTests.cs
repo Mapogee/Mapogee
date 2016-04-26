43489d7aab36c6e346dc365a72b58b9f1f95518a
pauldendulk <pauldendulk@gmail.com>
2016-04-26 09:19:37 +0200

    Adding unit test project

---
+++ tests/Mapogee.Tests/LayerCollectionTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace Mapogee.Tests
+{
+    [TestFixture]
+    public class LayerCollectionTests
+    {
+        [Test]
+        public void TesAddLayer()
+        {
+            // arrange
+            var layerCollection = new LayerCollection();
+            
+            // act
+            layerCollection.Add(new BaseLayer());
+
+            // assert
+            Assert.AreEqual(1, layerCollection.Count);
+
+        }
+    }
+}
