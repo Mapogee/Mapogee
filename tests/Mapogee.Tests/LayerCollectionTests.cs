ec37abf4bedc23678aaa867d048723f16fccc70d
pauldendulk <pauldendulk@gmail.com>
2016-04-26 18:32:12 +0200

    More tests

--- tests/Mapogee.Tests/LayerCollectionTests.cs
+++ tests/Mapogee.Tests/LayerCollectionTests.cs
@@ -6,17 +6,40 @@ namespace Mapogee.Tests
     public class LayerCollectionTests
     {
         [Test]
-        public void TesAddLayer()
+        public void TestAddLayer()
         {
             // arrange
             var layerCollection = new LayerCollection();
+            var layer = new BaseLayer();
+            ILayer addedLayer = null;
+            layerCollection.LayerAdded += (sender, args) => addedLayer = args.Value;
             
             // act
-            layerCollection.Add(new BaseLayer());
+            layerCollection.Add(layer);
 
             // assert
             Assert.AreEqual(1, layerCollection.Count);
+            Assert.AreEqual(layer, addedLayer);
 
         }
+
+        [Test]
+        public void TestRemoveLayer()
+        {
+            // arrange
+            var layerCollection = new LayerCollection();
+            ILayer notifiedLayer = null;
+            var layer = new BaseLayer();
+            layerCollection.Add(layer);
+            layerCollection.LayerRemoved += (sender, args) => notifiedLayer = args.Value;
+
+            // act
+            layerCollection.Remove(layer);
+
+            // assert
+            Assert.AreEqual(0, layerCollection.Count);
+            Assert.AreEqual(notifiedLayer, layer);
+        }
+
     }
 }
